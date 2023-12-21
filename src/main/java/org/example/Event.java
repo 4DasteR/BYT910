a069f34a49ae8c249851b597fe64cc759212d24e
4DasteR <66702087+4DasteR@users.noreply.github.com>
2023-12-21 14:15:48 +0100

    Event composition fixed. I think :3

--- src/main/java/org/example/Event.java
+++ src/main/java/org/example/Event.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class Event {
     private String name;
     private List<Object> attributes;
+    private Game game;
 
-    public Event(String name){
+    public Event(String name, Game game){
         this.name = name;
+        this.game = game;
         this.attributes = new ArrayList<>();
     }
 
