1e37f76d08e905766da461348793a3de6f609a56
4DasteR <66702087+4DasteR@users.noreply.github.com>
2023-12-21 14:22:14 +0100

    Event composition fixed. I think :3

--- src/main/java/org/example/Event.java
+++ src/main/java/org/example/Event.java
@@ -8,18 +8,19 @@ public class Event {
     private List<Object> attributes;
     private Game game;
 
-    public Event(String name, Game game){
+    public Event(String name, Game game) {
+        if (game == null) throw new IllegalArgumentException("Game object cannot be null");
         this.name = name;
         this.game = game;
         this.attributes = new ArrayList<>();
     }
 
-    public Event(String name, List<Object> attributes){
-        this(name);
+    public Event(String name, Game game, List<Object> attributes) {
+        this(name, game);
         this.attributes = attributes;
     }
 
-    public void create(Game game){
+    public void create(Game game) {
         //Here will be adding event to the game
     }
 }
